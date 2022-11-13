e5e06f550741a1e322c285538f0b2c86615fc7cb
CarlDesigner <krlitosfajardo626@gmail.com>
2022-11-13 16:32:24 -0500

    Mejora en las tarjetas

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,9 +1,9 @@
 function Cards(props) {
     return (
         <div>
-            <div className="container px-3 py-3" />
+            <div className="container px-5 py-3" />
             <h2 className="pb-2 border-bottom text-center">MODA URBANA 100% COLOMBIANA</h2>
-            <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-3 justify-content-center">
+            <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-5 py-5 justify-content-center">
 
                 <div className="col mx-4">
                     <div className="card card-cover overflow-hidden  rounded-4 shadow-lg">
