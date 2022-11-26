3a95481bce9fd03f190529e7bde5ad85bfd2d688
Isaac Benitez <benitezisaac421@gmail.com>
2022-11-26 18:37:09 -0500

    Register form styled

--- src/requests/createUser.jsx
+++ src/requests/createUser.jsx
@@ -5,13 +5,10 @@ async function createUser(user) {
     let { data } = await axios({
         method: 'post',
         baseURL: 'https://moiracoutureback-production.up.railway.app/api/users',
-        headers: {
-            
-        },
         data: user
 
     })
-    console.log(data)
+
     return data
 }
 
